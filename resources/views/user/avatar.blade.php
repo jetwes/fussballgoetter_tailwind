569347ff6af9626590f3458f2b2c2664be640067
Jens Twesmann <j.twesmann@star-media.biz>
2020-08-17 14:22:57 +0200

    changed avatar size

--- resources/views/user/avatar.blade.php
+++ resources/views/user/avatar.blade.php
@@ -18,8 +18,8 @@
                                 Foto
                             </label>
                             <div class="mt-2 flex items-center">
-                                <span class="h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                                  <img src="{{ auth()->user()->avatar }}" class="h12 w-12 rounded-full" alt="Avatar"/>
+                                <span class="h-20 w-20 rounded-full overflow-hidden bg-gray-100">
+                                  <img src="{{ auth()->user()->avatar }}" class="h20 w-20 rounded-full" alt="Avatar"/>
                                 </span>
                                 <span class="ml-5 rounded-md shadow-sm">
                                   <input type="file" name="avatar" class="py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out"/>
