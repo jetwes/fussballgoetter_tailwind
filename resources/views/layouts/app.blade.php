db263859f167d587b630e5a25ab41c9559d22851
Jens Twesmann <j.twesmann@star-media.biz>
2020-08-18 12:57:59 +0200

    test autoload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,15 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.0.1/dist/alpine.js" defer></script>
+    <script>
+        function handleVisibilityChange(){
+            if(document.visibilityState === "visible"){
+                load_container();
+            }
+        }
+
+        document.addEventListener('visibilitychange', handleVisibilityChange);
+    </script>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
