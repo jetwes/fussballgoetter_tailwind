178ccd321fe48e97f95241fad554b40f3bc1a15a
Jens Twesmann <j.twesmann@star-media.biz>
2020-08-28 09:23:33 +0200

    added liviwire 419 message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,8 +15,9 @@
     <script>
         window.livewire.onError(statusCode => {
             if (statusCode === 419) {
-                alert('Es gibt neue Inhalte - du wirst auf die aktuelle Version geleitet.')
+                //alert('Es gibt neue Inhalte - du wirst auf die aktuelle Version geleitet.')
                 window.location.reload();
+                return false;
             }
         });
     </script>
