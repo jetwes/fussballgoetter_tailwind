f614b615026ce239def969f5db2fbb1b3a46034f
Jens Twesmann <j.twesmann@star-media.biz>
2021-09-17 10:03:08 +0200

    chanegd route to maifeld

--- resources/views/livewire/practise.blade.php
+++ resources/views/livewire/practise.blade.php
@@ -123,6 +123,7 @@
                             </div>
                         </div>
 
+                        <!--
                         <div class="grid grid-cols-2 mt-4">
                             @if(!$beer && isset($this->participation) && $this->participation->participate)
                                 <h3 class="mb-4 col-span-1">Ich bringe Bier mit:</h3>
@@ -143,6 +144,7 @@
                                 @endif
                             @endif
                         </div>
+                        -->
 
                         @if($this->participation && $this->participation->participate)
                             <div>
