5f558d117918457e9ecb9224ed8722e3833472f6
Jens Twesmann <j.twesmann@star-media.biz>
2023-02-28 16:40:20 +0100

    updated app

--- resources/views/livewire/practise.blade.php
+++ resources/views/livewire/practise.blade.php
@@ -192,6 +192,7 @@
                                     </div>
                                     <h3 class="mt-4 text-sm text-gray-700 inline-flex justify-center items-center">
                                         @if($participator->participate)
+                                            {{ $loop->index + 1 }}
                                             <span class="mr-2 text-green-600">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
                                                   <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
